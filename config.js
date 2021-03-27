772e378dde9c1388184c9d5cf6bca11ad49c5160
Nithya1625 <70477925+Nithya1625@users.noreply.github.com>
2021-03-27 18:07:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import  firebase from 'firebase'
+require ('@firebase/firestore')
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8uQYklO5tDSiXd_E-PBpoarce2zWh9lA",
+    authDomain: "booksanta-ed28e.firebaseapp.com",
+    projectId: "booksanta-ed28e",
+    storageBucket: "booksanta-ed28e.appspot.com",
+    messagingSenderId: "582924705397",
+    appId: "1:582924705397:web:9662c33456a36bf117e23c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
